e268f365ce955b019dd836c20b86b8dc656c79e5
mwas <francismwangi152@gmail.com>
2016-07-23 10:11:39 +0300

    fixed manage.py command

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 # debugging with default server uncomment this and comment the gunicorn one
-python manage.py runserver 0.0.0.0:80--settings=$DJANGO_SETTINGS_MODULE
+python manage.py runserver 0.0.0.0:80 --settings=$DJANGO_SETTINGS_MODULE
 
 #exec  gunicorn --bind=0.0.0.0:80 gsma_hackathon.wsgi \
 #        --workers=5\
