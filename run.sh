1e5230adaa2ce584dac39f6c3418393efbad24e5
mwas <francismwangi152@gmail.com>
2016-07-23 10:03:03 +0300

    refactoring how to run the containers

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 # debugging with default server uncomment this and comment the gunicorn one
-#python manage.py runserver 0.0.0.0:3000 --settings=$DJANGO_SETTINGS_MODULE
+python manage.py runserver 0.0.0.0:80--settings=$DJANGO_SETTINGS_MODULE
 
-exec  gunicorn --bind=0.0.0.0:80 gsma_hackathon.wsgi \
-        --workers=5\
-        --log-level=info \
-        --log-file=-\
-        --access-logfile=-\
-        --error-logfile=-\
-        --timeout 30\
-        --reload
+#exec  gunicorn --bind=0.0.0.0:80 gsma_hackathon.wsgi \
+#        --workers=5\
+#        --log-level=info \
+#        --log-file=-\
+#        --access-logfile=-\
+#        --error-logfile=-\
+#        --timeout 30\
+#        --reload
